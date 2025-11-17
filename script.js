1be68680869c4531ad2afcc1b42bcf2274e5c36a
Abdi Deresa <abdishegute@gmail.com>
2025-11-16 19:00:46 -0800

    added additional css rules

---
+++ script.js
@@ -0,0 +1,4 @@
+//get the input, button and taskList
+const input=document.getElementById("task-input");
+const addBtn=document.getElementById("add-btn");
+const taskList=document.getElementById("task-list");
