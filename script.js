d85a3167136b646a7ff709a0526765ce43512a49
Abdi Deresa <abdishegute@gmail.com>
2025-11-16 19:05:16 -0800

    added js functionality

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 const input=document.getElementById("task-input");
 const addBtn=document.getElementById("add-btn");
 const taskList=document.getElementById("task-list");
+//get the tasks from local storage, i.e the browser
+const tasks=JSON.parse(localStorage.getItem("tasks"))  || [];
